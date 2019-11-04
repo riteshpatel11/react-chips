df95878e7b8d6453b4146995a57c90e64c5b047f
riteshpatel11 <56127640+riteshpatel11@users.noreply.github.com>
2019-11-05 02:05:12 +0530

    Implemented Functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const M = window.M;
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".chips");
+      var instances = M.Chips.init(elems, {
+        autocompleteOptions: {
+          data: {
+            Apple: null,
+            Bananas: null,
+            Grapes: null,
+            Mango: null,
+            Watermelon: null,
+            Oranges: null,
+            Raspberries: null,
+            Strawberries: null,
+            Kiwi: null
+          },
+          limit: Infinity,
+          minLength: 1
+        }
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Fruits</h1>
+          <div className="chips chips-autocomplete">
+            <input placeholder="Enter text"></input>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
